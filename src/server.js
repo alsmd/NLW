23042ef694047963c9922de3d276ac6aa8574b5a
Celebro01 <dangerveneta@gmail.com>
2020-08-06 12:21:06 -0300

    starting the server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
-require('express')()
+const express = require('express');
+const server = express();
+
+server.use(express.static("public"))                            //uma configuração do servidor // arquivos static sao arquivos como css/js/images
 .get("/",(req, res) => { //req/ requisição vai me trazer mais informaçoes // res/resposta é oque vai mostrar pra min em tela
-    return res.send("HI FROM NWL")                                // quando eu entrar na "/" essa função ira me retornar uma resposta
+    return res.sendFile(__dirname + "/views/index.html")                                // quando eu entrar na "/" essa função ira me retornar uma resposta
                                                                 // "/" é a rota
 
 })
-.get("/study",(req,res) => {
+.get("/estudar",(req,res) => {
 
-    return res.send("hi from study")
+    return res.sendFile(__dirname + "/views/estudar.html")
+
+})
+.get("/darAulas",(req,res) => {
+
+    return res.sendFile(__dirname + "/views/darAulas.html")
 
 })
 .listen(5500)
