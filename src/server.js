1788bad82304cdeeb3596bf63f4e5586ae780329
Celebro01 <dangerveneta@gmail.com>
2020-08-06 14:02:48 -0300

    starting the nunjucks config

--- src/server.js
+++ src/server.js
@@ -1,20 +1,65 @@
+//Estruturas de dados
+
+const proffys = [ //estrutura de dados
+
+    { 
+    name:"Diego Fernandes",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",whatsapp: "960223654",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Quimica",
+    cost: "20",
+    weekday: [0],
+    time_from:[720],
+    time_to:[1220]
+},
+{ 
+    name:"Daniele Evangelista",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",whatsapp: "960223654",
+    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Quimica",
+    cost: "20",
+    weekday: [1],
+    time_from:[720],
+    time_to:[1220]
+}
+
+
+]; //cada proffy é um objeto dentro de uma lista de objetos
+
+
+//Functions
+
+//Requisições
+function pageLanding(req,res){//req/ requisição vai me trazer mais informaçoes // res/resposta é oque vai mostrar pra min em tela 
+    return res.render("index.html");
+}
+
+function pageStudy(req,res){
+    return res.render("estudar.html");
+}
+
+function pageGiveClasses(req,res){
+    return res.render("darAulas.html");
+}
+
+
 const express = require('express');
 const server = express();
 
-server.use(express.static("public"))                            //uma configuração do servidor // arquivos static sao arquivos como css/js/images
-.get("/",(req, res) => { //req/ requisição vai me trazer mais informaçoes // res/resposta é oque vai mostrar pra min em tela
-    return res.sendFile(__dirname + "/views/index.html")                                // quando eu entrar na "/" essa função ira me retornar uma resposta
-                                                                // "/" é a rota
+//Configurando nunjucks
+const nunjucks = require('nunjucks');
 
+nunjucks.configure('src/views', {
+    express:server,
+    noCache: true,
 })
-.get("/estudar",(req,res) => {
 
-    return res.sendFile(__dirname + "/views/estudar.html")
 
-})
-.get("/darAulas",(req,res) => {
 
-    return res.sendFile(__dirname + "/views/darAulas.html")
 
-})
+//Configuração do servidor
+server.use(express.static("public"))   // arquivos static sao arquivos como css/js/images     
+.get("/", pageLanding)// quando eu entrar na "/" essa função ira me retornar uma resposta "/" é a rota) //uma configuração do servidor     
+.get("/estudar",pageStudy)
+.get("/darAulas",pageGiveClasses)
 .listen(5500)
